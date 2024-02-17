1e948026ee5ee21987fadf96ca78e001a270358d
SofaPl <sofiplotnikova04@gmail.com>
2024-02-17 14:56:47 +0300

    final commit

---
+++ lab1test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab1test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
